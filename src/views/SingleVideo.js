e11d28c7be4920bea5059e3e76f273ddfd1a4b08
dblodorn <dain@db13.us>
2019-02-25 13:21:09 -0800

    single video meta stuff

--- src/views/SingleVideo.js
+++ src/views/SingleVideo.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { VideoSingle, pageData } from './../components'
+import { VideoSingle, Head, pageData } from './../components'
 
 export default pageData((props) => {
+  console.log(props.project)
   return (
-    <VideoSingle data={props.project}/>
+    <React.Fragment>
+      <Head title={props.project.title} description={`${props.project.song_title} | ${props.project.title} | ${props.project.taxonomies.brand[0]}`} />
+      <VideoSingle data={props.project} />
+    </React.Fragment>
   )
 })
