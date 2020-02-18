2a6d5ffcd20b19d33c6235d72a53de9050affd8d
Dain Blodorn Kim <dainblodornkim@dains-imac.lan>
2020-02-18 13:23:48 -0800

    fetch from api no staging

--- src/controllers/fetchWpDataController.js
+++ src/controllers/fetchWpDataController.js
@@ -4,7 +4,7 @@ export default () => {
   return new Promise((resolve, reject) => {
     fetch((process.env.NODE_ENV === 'development')
       ? 'https://dmbk.network/gems/wp-json/api/v1/data/' 
-      : `data.${__webpack_hash__}.json`,
+      : 'https://dmbk.network/gems/wp-json/api/v1/data/',
       { method: 'GET' })
         .then(res => resolve(res))
         .catch(err => reject(err))
